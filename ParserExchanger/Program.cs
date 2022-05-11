2f1e52a3730ccbf7e4907205b22655c380a2d7d2
ThadeI0 <38001546+ThadeI0@users.noreply.github.com>
2022-05-11 14:49:08 +0300

    Update Program.cs
    trying ssl fix

--- ParserExchanger/Program.cs
+++ ParserExchanger/Program.cs
@@ -7,6 +7,14 @@ namespace ParserExchanger
     {
         static void Main(string[] args)
         {
+            
+            var EndPoint = "https://192.168.0.1/api";
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+            {
+                return true;
+            };
+            httpClient = new HttpClient(httpClientHandler) { BaseAddress = new Uri(EndPoint) };
             Connector connector = new Connector();
             connector.YouTrackRestParams();
             connector.YouTrackConnect();
