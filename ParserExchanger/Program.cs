f537473d9ee488e2a6a018fd1784597c3f6a6d58
ThadeI0 <38001546+ThadeI0@users.noreply.github.com>
2022-05-11 14:55:49 +0300

    Update Program.cs

--- ParserExchanger/Program.cs
+++ ParserExchanger/Program.cs
@@ -1,21 +1,10 @@
 using System;
-using System.Net;
 using YouTrackHubExchanger;
 
 namespace ParserExchanger
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
-            var EndPoint = "https://192.168.0.1/api";
-            var httpClientHandler = new HttpClientHandler();
-            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
-            {
-                return true;
-            };
-            httpClient = new HttpClient(httpClientHandler) { BaseAddress = new Uri(EndPoint) };
             Connector connector = new Connector();
             connector.YouTrackRestParams();
             connector.YouTrackConnect();
